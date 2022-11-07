a308cfd89448e7ef647d8c5a01436e27a2a1c02e
c0pp1 <albertocoppi@outlook.it>
2022-11-07 19:33:40 +0100

    ex02 done

--- Exercise02/complex_matrix.f90
+++ Exercise02/complex_matrix.f90
@@ -3,7 +3,8 @@ module complex_matrix
 
     implicit none
 
-    logical, private :: debug = .True.
+    logical, private :: debug = .True.  !< private logical variable to enable debugging within this module
+    private          :: Trace, MatAdjoint, Init_elems, Init_size
 
     !< define new type complex matrix
     !!
@@ -12,12 +13,14 @@ module complex_matrix
     !! size of the matrix end set the elemnts to zero, Init_elems take care of
     !! initializing all the variables of the cmatrix type, including trace and 
     !! ajugate if possible.
+    !! Base operations provaded are .Adj. to calculate the adjoint and .Tr. to
+    !! calculate the trace
     !!
     type :: cmatrix
-        double complex, allocatable :: elems(:, :)
-        integer, dimension(2)       :: MN = (/0,0/)
-        double complex              :: tr
-        double complex, allocatable :: adj_elems(:, :)
+        double complex, allocatable :: elems(:, :)      !< elements of the matrix
+        integer, dimension(2)       :: MN = (/0,0/)     !< size of matrix
+        double complex              :: tr               !< trace
+        double complex, allocatable :: adj_elems(:, :)  !< elements of the adjugate matrix
         
     end type cmatrix
 
@@ -29,6 +32,15 @@ module complex_matrix
         module procedure MatAdjoint
     end interface
 
+    !> Two possible initilizations:
+    !! - Init_size is a dummy initialization, initialize MN, allocate space for
+    !!   elems and initialize it to zero
+    !! - Init_elems initializes all the data structure, given elems, calculating 
+    !!   also the trace and the adjugate matrix
+    !!
+    !! If a variable is dummy initialized you have to manually deallocate elems
+    !! before using Init_elems
+    !!
     interface Init
         module procedure Init_size
         module procedure Init_elems 
@@ -39,13 +51,15 @@ module complex_matrix
         subroutine Init_size(self, MN, error)
             type(cmatrix)                     :: self
             integer, dimension(2), intent(in) :: MN
-            logical, intent(out), optional    :: error
+            logical, intent(out), optional    :: error  !< optional variable to track down errors
             
             if (present(error)) then
                 error = .False.
             end if
 
             call checkpoint(debug, string="Called Init_size")
+
+            ! check if already initialized, otherwise raise an error
             if ((self%MN(1) /= 0) .and. (self%MN(2) /= 0)) then
                 if (present(error)) then
                     error = .True.
@@ -53,7 +67,7 @@ module complex_matrix
                 print *, "Dimensions already initialized!"
             else
                 self%MN = MN
-                allocate(self%elems(MN(1), MN(2)), self%adj_elems(MN(1), MN(2)))
+                allocate(self%elems(MN(1), MN(2)))
                 self%elems = (0._8, 0.)
             end if
              
@@ -62,7 +76,7 @@ module complex_matrix
         subroutine Init_elems(self, elems, error)
             type(cmatrix), intent(inout)   :: self
             double complex, intent(in)     :: elems(:,:)
-            logical, intent(out), optional :: error
+            logical, intent(out), optional :: error         !< optional variable to track down errors
             integer, dimension(2)          :: MN
 
             call checkpoint(debug, string="Called Init_elems")
@@ -73,6 +87,7 @@ module complex_matrix
                 error = .False.
             end if
 
+            ! check if already initialized, otherwise raise an error
             if (allocated(self%elems)) then
                 print *, "Error: elements already initialized!"
                 if (present(error)) then
@@ -82,6 +97,8 @@ module complex_matrix
                 allocate(self%elems(MN(1), MN(2)))
                 self%elems = elems
                 self%MN = MN
+
+                ! only if possible also calculate trace and adjugate
                 if (MN(1) == MN(2)) then
                     allocate(self%adj_elems(MN(1), MN(2)))
                     self%tr = Trace(self)
@@ -90,70 +107,93 @@ module complex_matrix
             end if
         end subroutine
 
+        !> calculate trace of square matrix
+        !!
+        !! due to IO limits cannot call this function directly in
+        !! a IO operation; declare a variable to store its result 
+        !! and use the variable for IO operations instead
+        !!
         function Trace(matrix) result(tr)
             type(cmatrix), intent(in) :: matrix
             double complex            :: tr
             integer                   :: ii
 
+            ! check for non-square matrix
             if (matrix%MN(1) /= matrix%MN(2)) then
-                print *, "Error: The trace of a non square matrix doesn't exist!"
+                write (*,*) "Error: The trace of a non square matrix doesn't exist!"
                 return
-            else 
-                tr = (0._8, 0.)
-                do ii = 1, matrix%MN(1)
-                    tr = tr + matrix%elems(ii, ii)
-                end do
             end if
+            
+            tr = (0._8, 0.)
+            do ii = 1, matrix%MN(1)
+                tr = tr + matrix%elems(ii, ii)
+            end do
         end function Trace
 
+        !> slice input matrix elements
+        !!
+        !! takes matrix elements and returns matrix element without
+        !! selected lines and rows
+        !!
         function slice(matrix_elems, i, j) result(sl_elems)
             double complex, intent(in)  :: matrix_elems(:, :)
             integer, intent(in)         :: i, j
             double complex, allocatable :: sl_elems(:, :)
             logical, allocatable        :: mask(:, :)
-            integer :: m, n
+            integer :: mm, nn
 
-            m = ubound(matrix_elems, 1)
-            n = ubound(matrix_elems, 2)
-            allocate(mask(m, n))
-            allocate(sl_elems(m-1, n-1))
+            mm = ubound(matrix_elems, 1)
+            nn = ubound(matrix_elems, 2)
+            allocate(mask(mm, nn))
+            allocate(sl_elems(mm-1, nn-1))
 
             mask = .true.
             mask(i, :) = .false.
             mask(:, j) = .false.
 
-            sl_elems = reshape(pack(matrix_elems, mask), (/m-1, n-1/))
+            sl_elems = reshape(pack(matrix_elems, mask), (/mm-1, nn-1/))
             deallocate(mask)
         end function
 
+        !> calculate the determinant recursively
+        !!
+        !! calculate determinant slicing along the first column.
+        !! due to IO limits cannot call this function directly in
+        !! a IO operation; declare a variable to store its result 
+        !! and use the variable for IO operations instead.
+        !!
         recursive function Determinant(matrix_elems) result(det)
             double complex, intent(in) :: matrix_elems(:, :)
             double complex             :: det
-            integer                    :: m, n, jj
+            integer                    :: MN(2), jj
 
-            m = ubound(matrix_elems, 1)
-            n = ubound(matrix_elems, 2)
-            if (m /= n) then
+            MN = (/ubound(matrix_elems, 1), ubound(matrix_elems, 2)/)
+            if (MN(1) /= MN(2)) then
                 print *, "Error: The determinant of a non square matrix doesn't exist!"
                 return
-            end if
-
-            if (n == 1) then
+            else if (MN(1) == 1) then
                 det = matrix_elems(1, 1)
             else
                 det = (0._8, 0)
-                do jj = 1, n
+                do jj = 1, MN(1)
                     det = det + (-1._8)**(1+jj)*matrix_elems(1, jj)*Determinant(slice(matrix_elems, 1, jj))
                 end do
             end if
         end function
 
+        !> get the cofactor matrix, useful for the adjoint
+        !!
+        !! due to IO limits cannot call this function directly in
+        !! a IO operation; declare a variable to store its result 
+        !! and use the variable for IO operations instead
+        !!
         function Cofactor(matrix_elems, MN) result(cofact)
             double complex, intent(in)  :: matrix_elems(:, :)
             integer, intent(in)         :: MN(2)
             integer                     :: ii, jj
             double complex, allocatable :: cofact(:, :)
 
+            ! check for non-square matrix
             if (MN(1) /= MN(2)) then
                 print *, "Error: The cofactor of a non square matrix doesn't exist!"
                 return
@@ -176,5 +216,19 @@ module complex_matrix
             call Init(adjoint, matrix%MN)
             adjoint%elems = transpose(Cofactor(matrix%elems, matrix%MN))
         end function MatAdjoint
+
+        subroutine writecmatrix(matrix, unit)
+            type(cmatrix), intent(in) :: matrix
+            integer, intent(in)       :: unit
+            integer                   :: ii, jj
+
+            do ii = 1, matrix%MN(1)
+                do jj = 1, matrix%MN(2)
+                    write (unit, '(ES12.6, A, ES12.6, TR4)', advance='no') &
+                        real(matrix%elems(ii, jj)), ' +i', aimag(matrix%elems(ii, jj))
+                end do
+                write (unit, *)
+            end do
+        end subroutine
 
 end module
--- Exercise02/exercise02c.f90
+++ Exercise02/exercise02c.f90
@@ -3,14 +3,59 @@ program main
     use checkpoint_mod
     use advanced_ops
 
-    type(cmatrix) :: matrix
-    integer, dimension(2) :: dims = (/1,2/)
-    double complex :: elems
-    logical :: debug = .True.
-    logical :: error
+    type(cmatrix)         :: matrix
+    type(cmatrix)         :: adjoint
+    integer, dimension(2) :: dims = (/1,2/), dims2 = (/3,3/)
+    integer               :: counter1, unit = 100
+    double complex, allocatable :: elems(:, :)
+    logical               :: debug = .True.
+    logical               :: error
+    double complex        :: tr, det
 
-    call checkpoint(debug, vector=real(dims))
+    ! initialize matrix elements
+    allocate(elems(dims2(1), dims2(2)))
+    elems = reshape((/(2d0, 1.), (2d0, 3.), (4d0, 5.), (2.5d0, 3.2)/), shape=dims2, &
+                    pad=(/(4d0, 3.), (7d0, 2.2), (3d0, 5.1)/), order=(/2, 1/))
+
+    call checkpoint(debug, string="dummy initilization with size:" ,vector=real(dims))
+    ! check dummy initialization
     call Init(matrix, dims, error)
 
-    print *, matrix%MN, error, matrix%elems
+    ! deallocate elems before initializing again
+    deallocate(matrix%elems)
+    ! check complete initialization
+    call Init(matrix, elems, error=error)
+
+
+    write (*, *) "Original matrix:"
+    do counter1 = 1, ubound(elems, 1)
+        write (*, *) matrix%elems(counter1, :)
+    end do
+    write (*, '(/)', advance='no')
+
+    tr = .Tr. matrix
+    det = Determinant(elems)
+
+    write (*, *) "Trace from new data structure: ", matrix%tr
+    print *, "Trace from operator: ", tr
+    write (*, *) "Determinant: ", det    
+
+    write (*, *) "Adjugate matrix from new data structure:"
+    do counter1 = 1, ubound(elems, 1)
+        write (*, *) matrix%adj_elems(counter1, :)
+    end do
+    write (*, '(/)', advance='no')
+
+    adjoint = .Adj. matrix
+
+    write (*, *) "Adjugate matrix from operator:"
+    do counter1 = 1, ubound(elems, 1)
+        write (*, *) adjoint%elems(counter1, :)
+    end do
+    write (*, '(/)', advance='no')
+
+    ! test writing on file
+    open(unit = unit, file="./prova.txt", action='write')
+    call writecmatrix(matrix, unit)
+    close(unit=unit)
 end program
