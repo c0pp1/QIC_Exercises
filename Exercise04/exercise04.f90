7f11fc657b53a1cd9b5eadd1435c02b3fec82002
c0pp1 <albertocoppi@outlook.it>
2022-11-28 22:15:01 +0100

    doing ex04

--- Exercise04/exercise04.f90
+++ Exercise04/exercise04.f90
@@ -1,7 +1,13 @@
 !> Harmonic oscillator
-!! get the first k eigenvalues and eigenvectors from
+!! get the first k_user eigenvalues and eigenvectors from
 !! a one dimensional harmonic oscillator
 !!  H = p**2 + w**2*q**2
+!!
+!! with "--precision" you can set the relative precision of the output
+!! eigenvalues. The relation between precision and the number of points
+!! used in discretization k has been deduced through graphical results
+!! studying the case k = k_user.
+!! N.B. this relation is valid only if precision < 3%
 !!
 program main
     use lapack_wrapper
@@ -10,9 +16,9 @@ program main
 
     integer :: ii, k, k_user, info, outunit=100
     double precision, allocatable :: x(:), H(:, :), eigenvals(:)
-    double precision :: dx, xlims(2) = (/-1.,1./), w = 1., hbar = 6.62d-34
+    double precision :: dx, xlims(2) = (/-1.,1./), w = 1., precision = 0.01
     logical :: skip_next = .false.
-    character(len=32) :: arg
+    character(len=32) :: arg, ofdir = './exercise04'
 
     do ii = 1, command_argument_count()
         if (skip_next) then
@@ -30,6 +36,16 @@ program main
             case ('-w', '--frequency')
                 call get_command_argument(ii+1, arg)
                 read (arg, *) w
+                skip_next = .true.
+
+            case ('-p', '--precision')
+                call get_command_argument(ii+1, arg)
+                read (arg, *) precision
+                skip_next = .true.
+
+            case ('-o', '--odirname')
+                call get_command_argument(ii+1, arg)
+                read (arg, '(A)') ofdir
                 skip_next = .true.
 
             case ('-x', '--xlims')
@@ -50,39 +66,39 @@ program main
         end select
     end do
 
-    !! multiply the number of eigenvalues we want by ten in order
-    !! to have enough evaliuation points
-    k = k_user * 10
+    ! this relation guarantees the precision the user wants
+    k = ceiling(real(k_user, kind=8) / (10._8*precision))
 
+    ! allocate memory for discretized space, hamiltonian, and eigenvalues
     allocate(x(k), H(k, k), eigenvals(k))
     dx = abs(xlims(2)-xlims(1))/real(k-1)
     x = (/(minval(xlims)+ii*dx, ii=0,k-1, 1)/)
 
-    call get_Hamiltonian(H, w, x)
+    call get_Hamiltonian(H, w, x, dx)
 
+    ! JOBZ='V' gives the eigenstates as columns in the input matrix
     call ev_wrap(JOBZ='V', A=H, W=eigenvals, INFO=info)
     if(info /= 0) then
         print *, "ERROR: failed to get eigenvalues"
         stop
     end if
 
-    print *, eigenvals(:k_user)
-
-    open(unit=outunit, file='./prova.txt', action='write')
+    open(unit=outunit, file=trim(ofdir) // '.txt', action='write')
     do ii=1, k
         write(outunit, *) H(ii, :)
     end do
+    write(outunit, *) eigenvals
     close(outunit)
 
     contains
 
         subroutine print_help()
             print *, "Usage: ./exercise04 -k [#eigen to calculate] -x [xlims, comma separated without blank spaces] -w [frequency]"
-        end subroutine
+        end subroutine print_help
 
-        subroutine get_Hamiltonian(A, w, x)
+        subroutine get_Hamiltonian(A, w, x, dx)
             double precision, intent(inout) :: A(:, :)
-            double precision, intent(in) :: w, x(:)
+            double precision, intent(in) :: w, x(:), dx
             integer :: m, n, ii, jj
 
             m = ubound(A, 1)
@@ -95,12 +111,23 @@ program main
 
             do ii=1, m
                 do jj = 1, n
-                    if(ii==jj) A(ii, jj) = 2. + w**2*x(ii)**2
-                    if((ii==jj+1) .or. (ii==jj-1)) A(ii, jj) = -1.
+                    if(ii==jj) A(ii, jj) = 1./dx**2 + potential(x(ii), w)/2
+                    if((ii==jj+1) .or. (ii==jj-1)) A(ii, jj) = -0.5/dx**2
                 end do
             end do
 
-            A = A*real(m-1)/(x(m) - x(1))
-        end subroutine
+        end subroutine get_Hamiltonian
+
+        function potential(x, w_user) result(V)
+            double precision :: x
+            double precision, intent(in), optional  :: w_user
+            double precision :: V
+            double precision :: w = 1._8
+
+            if(present(w_user)) w = w_user
+            
+            V = w**2*x**2
+
+        end function potential
 
 end program
